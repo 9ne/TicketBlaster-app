38c41a9120883a37510c95b5c5b221e7f7e9ac36
9ne <jajonaumoski@gmail.com>
2023-06-28 14:50:02 +0200

    footer+style,signup+style

--- web/src/components/App.jsx
+++ web/src/components/App.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { Navbar } from './Navbar';
+import { Footer } from './Footer';
 import { Login } from './Login';
+import { Createaccount } from './Createaccound';
 
 export const App = () => {
   return (
@@ -10,8 +12,10 @@ export const App = () => {
       <div>
         <Routes>
           <Route path='/login' element={<Login/>}></Route>
+          <Route path='/create-account' element={<Createaccount/>}></Route>
         </Routes>
       </div>
+      <Footer/>
     </div>
   );
 };
--- web/src/components/Login.jsx
+++ web/src/components/Login.jsx
@@ -9,16 +9,16 @@ export const Login = () => {
     <div id='login'>
       <h1 className='heading-log-in'>Log In</h1>
       <div className='login-width'>
-        <form action="" method='get'>
+        <form action="" method='post'>
           <label htmlFor="email" className="label-login login-label-style">Email</label>
-          <input type="email"  className="input-login login-input-style" required/>
+          <input type="email" name="email" id="email" className="input-login login-input-style" required/>
           <label htmlFor="password" className="label-login login-label-style">Password</label>
-          <input type="password" className="input-login login-input-style" required />
+          <input type="password" name="password" id="password" className="input-login login-input-style" required />
           <div className='flex-login'>
-            <Link className='link-forgot'>Forgot Password?</Link>
+            <Link to='/' className='link-forgot'>Forgot Password?</Link>
             <button className='log-in-button'>Log in</button>
           </div>
-          <Link to='#' className='link-dont'>Don't have account?</Link>
+          <Link to='/create-account' className='link-dont'>Don't have account?</Link>
         </form>
       </div>
     </div>
