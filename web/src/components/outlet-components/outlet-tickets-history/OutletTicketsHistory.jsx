7b6852e43ed66446c54fa9a655b840a0a7dab9d2
9ne <jajonaumoski@gmail.com>
2023-09-21 19:35:55 +0200

    popup tickets history done

--- web/src/components/outlet-components/outlet-tickets-history/OutletTicketsHistory.jsx
+++ web/src/components/outlet-components/outlet-tickets-history/OutletTicketsHistory.jsx
@@ -3,16 +3,24 @@ import { AuthContext } from '../../../Context/AuthContext';
 import { Link } from 'react-router-dom';
 import './outlet-tickets-history-style/outlet-tickets-history.css';
 import axios from 'axios';
+import qrImg from '../../logo/qr.png';
+import logoDark from '../../logo/logo-dark.png';
 
 export const OutletTicketsHistory = () => {
   const [tickets, setTickets] = useState([]);
   const { userId } = useContext(AuthContext);
+  const [popupPrint, setPopupPrint] = useState(false);
+  const [popupDetailsPrint, setPopupDetailsPrint] = useState(null);
+
+  const popupPrintClose = () => {
+    setPopupDetailsPrint(null);
+    setPopupPrint(false);
+  }
 
   const isEventOutdated = (eventDate) => {
     const currentDate = new Date();
     return currentDate > new Date(eventDate);
   }
-
 
   const getAllTickets = async () => {
     try {
@@ -50,7 +58,15 @@ export const OutletTicketsHistory = () => {
                   <p className="ticket-history-details">{ticket.eventDetails}</p>
                   <div className="ticket-history-location-button-flex">
                     <p className="ticket-history-details-location">{ticket.location}</p>
-                    <Link className="tickets-history-button-print">Print</Link>
+                    <Link 
+                      className="tickets-history-button-print" 
+                      onClick={() => {
+                        setPopupPrint(true);
+                        setPopupDetailsPrint(ticket);
+                      }}
+                      >
+                        Print
+                    </Link>
                   </div>
                 </div>
               </div>
@@ -58,6 +74,36 @@ export const OutletTicketsHistory = () => {
           })  
         }
       </div>
+      {popupPrint && popupDetailsPrint && (
+        <div className="popup-print" onClick={popupPrintClose}>
+          <div className="popup-print-flex">
+            <div className="popup-print-flex-top">
+              <div>
+                <img src={logoDark} alt="logo-dark"  className="popup-logo-dark"/>
+              </div>
+              <div>
+                <img src={`/images/${popupDetailsPrint.image}`} alt={popupDetailsPrint.name} className="popup-print-image" />
+              </div>
+            </div>
+            <div className="popup-print-flex-bottom">
+              <div className="popup-print-flex-bottom-inner">
+                <div className="popup-print-flex-bottom-inner-left">
+                  <p className="popup-print-title">{popupDetailsPrint.name}</p>
+                  <p className="popup-print-date">
+                    {new Date(popupDetailsPrint.date).toLocaleDateString('en-US', {
+                        year: 'numeric',
+                        month: 'long',
+                        day: 'numeric'
+                      })}
+                  </p>
+                  <p className="popup-print-location">{popupDetailsPrint.location}</p>
+                </div>
+                <img src={qrImg} alt="qr-code" className="image-qr" />
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   )
 };
